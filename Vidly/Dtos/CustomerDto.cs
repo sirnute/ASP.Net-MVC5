0b214b09b095a9812c3ac5166851c27895adbabf
sirnute <nuteempire@gmail.com>
2018-05-20 17:27:52 -0500

    Created a customer dto

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid Name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+        public byte MembershipTypeId { get; set; }
+
+        [Column(TypeName = "Date")]
+        [Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; }
+    }
+}
